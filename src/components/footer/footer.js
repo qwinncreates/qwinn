36793fe349f0c2534587afc6bbc7697fd5963454
jordanmceachern <jormceachern@gmail.com>
2018-12-14 20:15:52 +0000

    bug fixes

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,6 +4,8 @@ import Facebook from './facebook.png'
 import Instagram from './instagram.png'
 import Twitter from './twitter.png'
 
+console.log(Facebook)
+
 const Footer = () => {
     return (
         <div id="footer">
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { Link } from 'gatsby'
 import DarkMode from './darkMode/darkMode'
 import './layout.css'
+import facebook from './footer/facebook.png'
+import instagram from './footer/instagram.png'
+import twitter from './footer/twitter.png'
 
 class Layout extends Component {
 
@@ -36,7 +39,7 @@ render(){
                     href="https://www.facebook.com/BrittanyMcQuinn/?epa=SEARCH_BOX&jazoest=265100121975011957685684986995861021111128168708410148113117721019012172867710790103108121851101131144883110104815865100119116106118105525210273791225511648735711680867095805010410180571085179999974551101171091071101055112077119" 
                     target="_blank" 
                     rel="noopener noreferrer">
-                        <img id="fb" src="/facebook.png" alt="link to facebook"/>
+                        <img id="fb" src={facebook} alt="link to facebook"/>
                     </a>
                 </div>
                 <div className="links">
@@ -44,14 +47,14 @@ render(){
                     href="https://www.instagram.com/brittanymcquinn/?hl=en" 
                     target="_blank" 
                     rel="noopener noreferrer">
-                        <img src="/instagram.png" alt="link to instagram"/>
+                        <img src={instagram} alt="link to instagram"/>
                     </a>
                 </div>
                 <div className="links">
                     <a 
                     href="https://twitter.com/brittanymcquinn" 
                     target="_blank" rel="noopener noreferrer">
-                        <img src="/twitter.png" alt="link to twitter"/>
+                        <img src={twitter} alt="link to twitter"/>
                     </a>
                 </div>
                 <div className="links" id="buyDiv">

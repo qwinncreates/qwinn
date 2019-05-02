3d51c2e7670fe45b242d0c1e4e3017b414e9df6a
Jordan McEachern <jormceachern@gmail.com>
2019-05-02 16:39:53 -0300

    bleum events

--- src/pages/shows.js
+++ src/pages/shows.js
@@ -9,11 +9,11 @@ const Shows = () => {
             <div id="showsInfo">
                 <div>
                     <p>with Bleum:</p>
-                    <p>TBD</p>
+                    <p className='indent'><a href='https://www.facebook.com/pg/bleummusic/events/?ref=page_internal' target="_blank" rel="noopener noreferrer">[click to view]</a></p>
                 </div>
                 <div>
                     <p>Solo:</p>
-                    <p>TBD</p>
+                    <p className='indent'>TBD</p>
                 </div>
             </div>
         </div>

c368d75e4e1faf243c85f48b195fd2ac83105430
Jordan McEachern <jormceachern@gmail.com>
2019-05-02 16:41:23 -0300

    Add files via upload

--- src/components/imageLoader/imageLoader.js
+++ src/components/imageLoader/imageLoader.js
@@ -1,22 +1,46 @@
 import React from 'react'
 import './imageLoader.css'
-import svg from './brittany.svg'
-import jpg from './brittany.jpg'
+import pic from './brittany.jpg'
+import pic_l from './brittany_light.jpg'
+import pic_m from './brittany_mobile.jpg'
+import pic_ml from './brittany_mobile_light.jpg'
 
 class ImageLoader extends React.Component {
   state={
-    loadFirst: false
+    // loadFirst: false,
+    src: ''
   }
 
-  componentDidMount(){
-    const img = new Image()
+  componentWillMount(){
+    let isMobile
+    if (typeof window === 'undefined') {
+      isMobile = false
+    } else {
+      isMobile = window.matchMedia('(max-width: 900px)').matches
+    }
+    let jpg = pic
+    let jpg_l = pic_l
+    if (isMobile) {
+      jpg = pic_m
+      jpg_l = pic_ml
+    }
+
+    this.setState({src: jpg_l})
+
+    let img
+    if (typeof Image === 'undefined') {
+      img = {}
+    } else {
+      img = new Image()
+    }
     img.src = jpg
-    img.onload = () => {this.setState({loadFirst: true})}
+    // img.onload = () => {this.setState({loadFirst: true, src: jpg})}
+    img.onload = () => {this.setState({src: jpg})}
   }
   
   render(){
-    const src = 
-    this.state.loadFirst ? jpg : svg
+    // src = this.state.loadFirst ? jpg : jpg_l
+    const src = this.state.src
     return (
           <div 
             className="background"

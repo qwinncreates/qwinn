03d58b177273dc3707d4b59754c8112e009b78eb
jordanmceachern <jormceachern@gmail.com>
2019-05-06 08:59:45 -0300

    removed sound of pop

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,10 +9,10 @@ const Contact = () => {
             <div id="contactInfo">
                 <div>
                     <p id="brittContact"><a href="mailto:contact@brittanymcquinn.com" target="_top">contact@brittanymcquinn.com</a></p>
-                    <p id="publish">Publishing / Sync - Sound of Pop:</p>
+                    {/* <p id="publish">Publishing / Sync - Sound of Pop:</p>
                     <p><a href="https://www.soundofpop.com" target="_blank" rel="noopener noreferrer">www.soundofpop.com</a></p>
                     <p>Glenn McMullen - <a href="mailto:Glenn@soundofpop.com" target="_top">glenn@soundofpop.com</a></p>
-                    <p>Tim Hardy - <a href="mailto:Tim@soundofpop.com" target="_top">tim@soundofpop.com</a></p>
+                    <p>Tim Hardy - <a href="mailto:Tim@soundofpop.com" target="_top">tim@soundofpop.com</a></p> */}
                 </div>
             </div>
         </div>

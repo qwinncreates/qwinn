fc2d313673767ef922cec25851fc7ba81d869140
jordanmceachern <jormceachern@gmail.com>
2019-05-06 09:24:28 -0300

    added site meta data in gatsby-config

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+      title: `Brittany McQuinn`,
+      siteUrl: `brittanymcquinn.com`,
+      description: `Brittany McQuinn is a singer songwriter from the province of New Brunswick in the East Coast of Canada.
+        Brittany works actively on solo projects, artist collaborations, and writing/singing lead for her duo with Daniel 
+        James in a band known as Bleum. Visit to read her story, find her music, or get connected with her business contact.`
+    },
+  }
